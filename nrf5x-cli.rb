e56b3579c146ed197abadcb309afaa01721953f0
Hearty, Oh <heartyoh@hatiolab.com>
2017-07-21 03:03:19 +0800

    text nrf5x-cli

--- nrf5x-cli.rb
+++ nrf5x-cli.rb
@@ -6,9 +6,11 @@ class Nrf5xCli < Formula
   sha256 "38a0c489777fbd633bd76710730a563058f17b94a8d5c7360c68c4aeda3be29e"
 
   def install
+    # lib.install Dir["nrfjprog/*.dylib"]
+    bin.install_symlink mergehex/mergehex
+    bin.install_symlink nrfjprog/nrfjprog
+    # bin.install "mergehex/mergehex"
+    # bin.install "nrfjprog/nrfjprog"
     prefix.install Dir["*"]
-    lib.install Dir["nrfjprog/*.dylib"]
-    bin.install "mergehex/mergehex"
-    bin.install "nrfjprog/nrfjprog"
   end
 end
