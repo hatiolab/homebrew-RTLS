06ca5388737f026243f5312160867ec3dc8a4b0e
Hearty, Oh <heartyoh@hatiolab.com>
2017-07-21 03:30:29 +0800

    cleaning

--- nrf5-sdk.rb
+++ nrf5-sdk.rb
@@ -1,7 +1,3 @@
-# Documentation: http://docs.brew.sh/Formula-Cookbook.html
-#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Nrf5Sdk < Formula
 
   homepage "https://www.nordicsemi.com/eng/Products/Bluetooth-low-energy/nRF5-SDK"
