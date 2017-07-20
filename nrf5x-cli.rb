978bb80a20a965c0c7cc639bc265429e9d5f6a04
Hearty, Oh <heartyoh@hatiolab.com>
2017-07-21 01:28:11 +0800

    nrf5x-cli version info.

--- nrf5x-cli.rb
+++ nrf5x-cli.rb
@@ -2,6 +2,7 @@ class Nrf5xCli < Formula
   desc ""
   homepage ""
   url "https://www.nordicsemi.com/eng/nordic/download_resource/53406/15/11972112/99977"
+  version "9.6.0"
   sha256 "38a0c489777fbd633bd76710730a563058f17b94a8d5c7360c68c4aeda3be29e"
 
   def install
