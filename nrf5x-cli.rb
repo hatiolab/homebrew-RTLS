e73441ec256b9b7e44fbf9c4ea1aa3f28c3a8971
Hearty, Oh <heartyoh@hatiolab.com>
2017-07-21 01:17:30 +0800

    formula nrf5x-cli

---
+++ nrf5x-cli.rb
@@ -0,0 +1,11 @@
+class Nrf5xCli < Formula
+  desc ""
+  homepage ""
+  url "https://www.nordicsemi.com/eng/nordic/download_resource/53406/15/11972112/99977"
+  sha256 "38a0c489777fbd633bd76710730a563058f17b94a8d5c7360c68c4aeda3be29e"
+
+  def install
+    bin.install 'mergehex/mergehex'
+    bin.install 'nrfjprog/nrfjprog'
+  end
+end
